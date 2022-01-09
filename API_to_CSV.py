c84c7d194961e8a1f8c82cf4d8e03033ea574a53
Martin <martin.79k@gmail.com>
2022-01-09 21:44:25 +0000

    scrapping data for 2021 split scrapping for each month (for loop) to avoid api call limits

--- API_to_CSV.py
+++ API_to_CSV.py
@@ -1,12 +1,14 @@
 from call_API import api_results
 import pandas as pd
 import time
+from pandas.tseries.offsets import MonthEnd
 
 # get movies for whole 2021
+movies_year = 2021
 
 # explore data
 # get list of movies from 2021
-movies_list = api_results('discover/movie', '&primary_release_year=2021')
+movies_list = api_results('discover/movie', '&primary_release_year=' + str(movies_year))
 
 # check how many pages in result (max page parameter is 1000)
 # https://developers.themoviedb.org/3/discover/movie-discover
@@ -33,22 +35,23 @@ def get_movies_list(_dateMin, _dateMax):
         _df = _df[['id', 'title', 'release_date', 'vote_average', 'vote_count']]
         _data = _data.append(_df)
         print("Page " + str(p+1) + " of " + str(_totalPages) + " scrapped")
-        # adding delay to prevent api requests limit
-        time.sleep(1)
     print(_data.shape)
     return _data
 
 # initialize empty dataframe
-movies_list = pd.DataFrame()
+df_movies_list = pd.DataFrame()
 
-# get movies list for the specific date range
-movies_list_1 = get_movies_list('2021-01-01', '2021-06-30')
-movies_list = movies_list.append(movies_list_1)
-
-movies_list_2 = get_movies_list('2021-07-01', '2021-12-31')
-movies_list = movies_list.append(movies_list_2)
+# scrapping for each month
+for m in range(12):
+    # get last day of the month
+    _month_lastDay = pd.to_datetime(str(movies_year) + '-' + str(format(m+1,'02d')), format="%Y-%m") + MonthEnd(0)
+    _movies_list = get_movies_list(str(movies_year) + '-' + str(format(m+1,'02d')) + '-01', str(format(_month_lastDay, "%Y-%m-%d")))
+    df_movies_list = df_movies_list.append(_movies_list)
+    print(df_movies_list.shape)
+    # adding delay to prevent api calls limit
+    time.sleep(10)
 
 # reset index for whole data range
-movies_list.reset_index(inplace=True, drop=True)
-movies_list.to_csv('Movies_list.csv')
-print(movies_list.shape)
+df_movies_list.reset_index(inplace=True, drop=True)
+df_movies_list.to_csv('movies_list_2021.csv')
+print(df_movies_list.shape)
