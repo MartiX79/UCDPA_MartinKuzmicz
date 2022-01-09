cc17856026687201ce7cd2472f79f6b996f9c66f
Martin <martin.79k@gmail.com>
2022-01-09 20:09:21 +0000

    loops and function for scrapping movies for whole 2021

--- API_to_CSV.py
+++ API_to_CSV.py
@@ -1,6 +1,10 @@
 from call_API import api_results
 import pandas as pd
+import time
 
+# get movies for whole 2021
+
+# explore data
 # get list of movies from 2021
 movies_list = api_results('discover/movie', '&primary_release_year=2021')
 
@@ -10,42 +14,41 @@ total_pages = movies_list['total_pages']
 print("Total pages for moves list: " + str(total_pages))
 
 # over 1000 pages in results
-# get list of all genres
-genres = api_results('/genre/movie/list')
-df_genre = pd.json_normalize(genres['genres'])
-print("Total number of genres: " + str(len(df_genre)))
+# split data into chunks
 
-# call movies for each genres - this will limit number of pages in each result
-# check how many pages
-genre_movies = api_results('discover/movie', '&primary_release_year=2021&with_genres=' + str(37))
-print(genre_movies['total_pages'])
+# create reusable function
+def get_movies_list(_dateMin, _dateMax):
+    _data = pd.DataFrame()
+    _movies_list = api_results('discover/movie', '&primary_release_date.gte=' + str(_dateMin) + '&primary_release_date.lte=' + str(_dateMax))
+    _totalPages = _movies_list['total_pages']
+    print("Date range: " + str(_dateMin) + " - " + str(_dateMax))
+    print("Total pages: " + str(_totalPages))
+    print("Number of results: " + str(_movies_list['total_results']))
+    # loop trough all pages
+    for p in range(_totalPages):
+        _movies_list = api_results('discover/movie', '&sort_by=release_date.desc&primary_release_date.gte=' + str(_dateMin) + '&primary_release_date.lte=' + str(_dateMax) + '&page=' + str(p + 1))
+        # created dataframe from results
+        _df = pd.json_normalize(_movies_list['results'])
+        # drop unneeded columns
+        _df = _df[['id', 'title', 'release_date', 'vote_average', 'vote_count']]
+        _data = _data.append(_df)
+        print("Page " + str(p+1) + " of " + str(_totalPages) + " scrapped")
+        # adding delay to prevent api requests limit
+        time.sleep(1)
+    print(_data.shape)
+    return _data
 
-# get result for each page
-_page = 1
-while _page <= genre_movies['total_pages']:
-    genre_movies_page = api_results('discover/movie', '&primary_release_year=2021&with_genres=' + str(37) + '&page=' + str(_page))
-    df_genre_movies_page = pd.json_normalize(genre_movies_page['results'])
-    print(df_genre_movies_page[['id', 'title', 'vote_average', 'vote_count']])
-    _page += 1
+# initialize empty dataframe
+movies_list = pd.DataFrame()
 
+# get movies list for the specific date range
+movies_list_1 = get_movies_list('2021-01-01', '2021-06-30')
+movies_list = movies_list.append(movies_list_1)
 
+movies_list_2 = get_movies_list('2021-07-01', '2021-12-31')
+movies_list = movies_list.append(movies_list_2)
 
-print('-----')
-
-
-'''
-for g in df_movies_genre['id']:
-    genre_movies = api_results('discover/movie', '&primary_release_year=2021&with_genres=' + str(28)).json()
-    print(genre_movies['results'])
-    print(genre_movies['total_results'])
-    print('-----')
-
-df = pd.json_normalize(movies_list['results'])
-print(df.shape)
-
-
-df = df[['id', 'title', 'vote_average', 'vote_count']]
-# df.to_csv('Movies_list.csv')
-print(df.shape)
-# print(df)
-'''
+# reset index for whole data range
+movies_list.reset_index(inplace=True, drop=True)
+movies_list.to_csv('Movies_list.csv')
+print(movies_list.shape)
