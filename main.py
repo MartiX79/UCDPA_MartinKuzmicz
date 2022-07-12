6fd57879737e500337f4a460005dd4f537c18926
Martin <martin.79k@gmail.com>
2022-07-12 20:12:18 +0100

    .

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from call_API import api_results
 import API_to_CSV
 
 
+'''
 # explore data
 # get list of movies from 2021 from API
 _, movies_list_results = api_results('/discover/movie', '&primary_release_year=2021')
@@ -19,37 +20,50 @@ print("Total pages for moves list: " + str(total_pages))
 # save discovered movies results to csv file
 #API_to_CSV.discoverMovies(2021, 'movies_list_2021')
 
-# get details for all mivies from the list and save to csv
+# get details for all movies from the list and save to csv
 API_to_CSV.get_move_details('movies_list_2021.csv', 'movies_list_2021_details')
-
+'''
 
 # get list of columns
 movies_df = pd.read_csv('movies_list_2021_details.csv')
 print(movies_df.columns)
 
-# keep only needed columns
+# drop unnecessary columns
 movies_df = movies_df[['id','genres','budget','revenue','vote_average','vote_count']]
 print(movies_df.columns)
-
-# analise data
-print(movies_df.info())
-print(movies_df.head())
 
 # change default display columns limit to see data from all columns
 pd.set_option('max_columns', None)
 print(movies_df.head())
 
-#replace 0 with NaN to indicate missing value and not the 0 revenue
+# replace 0 with NaN to indicate missing value and not the 0 revenue
 movies_df['revenue'] = movies_df['revenue'].replace(0, np.nan)
 movies_df['budget'] = movies_df['budget'].replace(0, np.nan)
+movies_df['vote_average'] = movies_df['vote_average'].replace(0, np.nan)
+movies_df['vote_count'] = movies_df['vote_count'].replace(0, np.nan)
 
+# calculate mean for votes
+vote_mean = movies_df['vote_average'].mean()
+vote_count_mean = movies_df['vote_count'].mean()
+
+# analise data
 print(movies_df.info())
+print(movies_df.head())
+print(vote_mean)
+print(vote_count_mean )
+
 
 # rating score weight (no. of vote_average / vote_count)
 movies_df['rating_score'] = movies_df['vote_average']/movies_df['vote_count']
 
+
+
+
+
+'''
 movies_df_vote = movies_df.loc[(movies_df != 0).all(axis=1), :]
 
 sn.histplot(data=movies_df_vote['vote_average'], bins=10)
-plt.show()
+#plt.show()
 
+'''
